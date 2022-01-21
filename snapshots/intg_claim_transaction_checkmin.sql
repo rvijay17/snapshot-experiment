3f23e152acad074ea8bcf106dbda7dda038467b1
Evan Williams <evan.williams@aginic.com>
2022-01-21 16:51:19 +1100

    variations on shapshots

---
+++ snapshots/intg_claim_transaction_checkmin.sql
@@ -0,0 +1,36 @@
+{% snapshot intg_claim_transaction_checkmin %}
+
+{{
+    config(
+      tags=["intgcheckmin"],
+      unique_key='claim_transaction_key',
+      target_schema='dev_evan',
+      strategy='check',
+      check_cols=['ts_updatetime', 't_updatetime', 'tl_updatetime'],
+      updated_at='updatetime'
+    )
+}}
+
+select concat(ts.id, '~', t.id, '~', tl.id) as claim_transaction_key
+    , ts.trandate   as trans_date
+    , ts.transet    as trans_set_type
+    , ts.userid     as trans_set_userid
+    , ts.updatetime as ts_updatetime
+
+    , t."type"      as trans_type
+    , t.auth        as trans_authorised
+    , t.updatetime  as t_updatetime
+
+    , tl."desc"     as trans_desc
+    , tl."amount"   as trans_amount
+    , tl.updatetime as tl_updatetime
+    , least(ts.dbt_updated_at, t.dbt_updated_at, tl.dbt_updated_at) as updatetime
+    
+from {{ ref('source_cc_transaction_set') }} ts 
+  left join {{ ref('source_cc_transaction') }} t on ts.id = t.transetid
+  left join {{ ref('source_cc_transaction_line') }} tl on t.id = tl.tranid
+where '2021-07-03 23:59:59' between ts.dbt_valid_from and coalesce(ts.dbt_valid_to,'9999-12-31 23:59:59')
+  and '2021-07-03 23:59:59' between t.dbt_valid_from  and coalesce(t.dbt_valid_to,'9999-12-31 23:59:59')
+  and '2021-07-03 23:59:59' between tl.dbt_valid_from and coalesce(tl.dbt_valid_to,'9999-12-31 23:59:59')
+
+{% endsnapshot %}
---
+++ snapshots/intg_claim_transaction_checkminbatch.sql
@@ -0,0 +1,38 @@
+{% snapshot intg_claim_transaction_checkminbatch %}
+
+{{
+    config(
+      tags=["intgcheckminbatch"],
+      unique_key='claim_transaction_key',
+      target_schema='dev_evan',
+      strategy='check',
+      check_cols=['ts_updatetime', 't_updatetime', 'tl_updatetime'],
+      updated_at='updatetime'
+    )
+}}
+-- use the timestamps to determine if there are any differences
+-- but, use the batch timestamp as the timestamp
+
+select concat(ts.id, '~', t.id, '~', tl.id) as claim_transaction_key
+    , ts.trandate   as trans_date
+    , ts.transet    as trans_set_type
+    , ts.userid     as trans_set_userid
+    , ts.updatetime as ts_updatetime
+
+    , t."type"      as trans_type
+    , t.auth        as trans_authorised
+    , t.updatetime  as t_updatetime
+
+    , tl."desc"     as trans_desc
+    , tl."amount"   as trans_amount
+    , tl.updatetime as tl_updatetime
+    , to_timestamp('2021-07-04 23:59:59', 'YYYY-MM-DD HH24:MI:SS')::timestamp as updatetime
+    
+from {{ ref('source_cc_transaction_set') }} ts 
+  left join {{ ref('source_cc_transaction') }} t on ts.id = t.transetid
+  left join {{ ref('source_cc_transaction_line') }} tl on t.id = tl.tranid
+where '2021-07-04 23:59:59' between ts.dbt_valid_from and coalesce(ts.dbt_valid_to,'9999-12-31 23:59:59')
+  and '2021-07-04 23:59:59' between t.dbt_valid_from  and coalesce(t.dbt_valid_to,'9999-12-31 23:59:59')
+  and '2021-07-04 23:59:59' between tl.dbt_valid_from and coalesce(tl.dbt_valid_to,'9999-12-31 23:59:59')
+
+{% endsnapshot %}
